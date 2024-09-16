b47803623427f311293ff61582eed2e9ca9298e6
jlaron230 <jlaron230@hotmail.fr>
2024-09-17 01:29:08 +0200

    fix debbug2

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://jeromegavinodev.com/',  // Remplacez par l'URL de votre site
+    generateRobotsTxt: true,  // Génère un fichier robots.txt
+    exclude: ['/privacy-legacy', '/private'],  // Exclure certaines pages du sitemap si nécessaire
+  }
