e0abb08228543b8fe5f328861826555a4348e5a9
jlaron230 <jlaron230@hotmail.fr>
2024-09-16 21:23:17 +0200

    ajout SEO

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+
+module.exports = {
+    siteUrl: 'https://portfolio-six-lake-46.vercel.app/', // Remplacez par l'URL de votre site
+    generateRobotsTxt: true, // Optionnel : génère un fichier robots.txt
+    // Vous pouvez ajouter d'autres options ici si nécessaire
+  };
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import './globals.css'; // Assure-toi que ce fichier existe et est correctement configuré
-import NavBarOverlay from './components/NavBar/NavBarOverlay'; // Vérifie le chemin
+import "./globals.css"; // Assure-toi que ce fichier existe et est correctement configuré
+import NavBarOverlay from "./components/NavBar/NavBarOverlay"; // Vérifie le chemin
 import ClickScrollBar from "./components/ScrollBar/ClickScrollBar"; // Vérifie le chemin
 import Footer from "./components/footer/Footer"; // Vérifie le chemin
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,13 +20,29 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Une description pour la page" />
-        <title>Document</title>
+        <meta
+          name="description"
+          content="Mes projets est compétences en informatique dans ce portfolio"
+        />
+        <meta name="author" content="Jérôme Gavino" />
+        <meta
+          property="og:image"
+          content="images/w1000-1133478AE9sXs77.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Jérôme propose ses compétences en informatique et ses qualités
+          de graphiste, modélisateur 3D, informaticien et ses compétences en développement informatique
+          pour vos besoins"
+        />
+        <meta property="og:title" content="Jérôme Gavino" />
+        <title>Jérôme Portfolio</title>
       </head>
       <body>
         <ClickScrollBar />
         <NavBarOverlay />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
