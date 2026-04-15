f2c613e402cf5dc40377fac933ab1df91e858510
jlaron230 <jlaron230@hotmail.fr>
2026-04-16 00:30:25 +0200

    deploy-vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local'
 import NavBarOverlay from "./components/NavBar/NavBarOverlay"; // Vérifie le chemin
 import ClickScrollBar from "./components/ScrollBar/ClickScrollBar"; // Vérifie le chemin
 import Footer from "./components/footer/Footer"; // Vérifie le chemin
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 export const metadata: Metadata = {
@@ -61,6 +62,7 @@ export default function RootLayout({
     <NavBarOverlay/>
     {children}
     <SpeedInsights />
+    <Analytics />
         <Footer />
       </body>
     </html>
